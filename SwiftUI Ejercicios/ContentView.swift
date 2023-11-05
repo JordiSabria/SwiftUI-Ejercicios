7f890846695dd2d68c209b0782f25994c589a39d
Jordi Sabrià Pagès <jordisabria@hotmail.com>
2023-11-05 11:09:12 +0100

    Initial Commit

---
+++ SwiftUI Ejercicios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI Ejercicios
+//
+//  Created by Jordi Sabrià Pagès on 5/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI Ejercicios/SwiftUI_EjerciciosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_EjerciciosApp.swift
+//  SwiftUI Ejercicios
+//
+//  Created by Jordi Sabrià Pagès on 5/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_EjerciciosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
